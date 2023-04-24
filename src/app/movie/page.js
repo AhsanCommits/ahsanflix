14c783bbdf8636ffb7a89edd5fb5703d34c8430e
Ahsan Raza <ahsanraza00059@gmail.com>
2023-04-24 10:28:41 +0500

    Fetch API Data in Next.js

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,10 +1,29 @@
 import Link from "next/link";
 
-const Page = () => {
+const Movie = async () => {
+
+
+    const url = process.env.RAPID_KEY;
+    const options = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/octet-stream',
+            'X-RapidAPI-Key': 'b14ded644cmshba74b7006afcde4p12dcafjsne74a7664bae6',
+            'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
     return (
         <>
             <h1>
-                Movie Page
+                Movie
             </h1>
             <Link href="/movie/asdf">
                 Go to
@@ -14,4 +33,4 @@ const Page = () => {
     );
 };
 
-export default Page;
+export default Movie;
