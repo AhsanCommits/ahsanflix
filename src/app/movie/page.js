18aa24fe2df693490d276cf7b6407a2d625cb8cc
Ahsan Raza <ahsanraza00059@gmail.com>
2023-04-24 13:23:40 +0500

    Api Data go through
    Api Data go through
    Movie Card Component
    Handling Image in Next.js
    Dynamic Movie Route (Blog)

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,33 +1,32 @@
-import Link from "next/link";
+import MovieCard from "@/app/components/MovieCard";
+import styles from "@/app/styles/common.module.css"
 
 const Movie = async () => {
 
-
     const url = process.env.RAPID_KEY;
+
     const options = {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/octet-stream',
+        method: 'GET', headers: {
             'X-RapidAPI-Key': 'b14ded644cmshba74b7006afcde4p12dcafjsne74a7664bae6',
             'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
         }
     };
 
-    try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
-    return (
-        <>
-            <h1>
-                Movie
-            </h1>
-            <Link href="/movie/asdf">
-                Go to
-            </Link>
+    const response = await fetch(url, options);
+    const data = await response.json();
+    const main_data = data.titles;
+
+    return (<>
+            <section className={styles.movieSection}>
+                <div className={styles.container}>
+                    <h1>Series & Movies</h1>
+                    <div className={styles.card_section}>
+                        {main_data.map((curElem) => {
+                            return <MovieCard key={curElem.id} {...curElem} />
+                        })}
+                    </div>
+                </div>
+            </section>
         </>
 
     );
