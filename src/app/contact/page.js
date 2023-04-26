0d1a17f26c442113659b4bf22931c499a0e5bd8a
Ahsan Raza <ahsanraza00059@gmail.com>
2023-04-26 13:06:26 +0500

    Contact Us page & React Icons
    Contact Card

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,13 @@
+import ContactCard from "@/app/components/ContactCard";
+import styles from './contact.module.css'
 
 const Page = () => {
-    return (
-        <div>
-<h1>Contact Page</h1>
-        </div>
-    );
+    return (<>
+            <div className={styles.container}>
+                <h1>Contact Page</h1>
+                <ContactCard/>
+            </div>
+        </>);
 };
 
 export default Page;
