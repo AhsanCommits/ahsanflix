6efcd53ec19155b04b47cdd7a62638b2c0fe3d80
Ahsan Raza <ahsanraza00059@gmail.com>
2023-04-29 23:48:21 +0500

    Create Database using MongoDB
    Create Database using MongoDB
    Connection to database

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -19,7 +19,7 @@ const Page = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109823.40662215475!2d73.01233412523428!3d30.662650655820855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3922b62cd8405a6d%3A0x6cce79c0f78cbfb7!2sSahiwal%2C%20Sahiwal%20District%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1682693036476!5m2!1sen!2s"
             width={100} height={450} style={{border:0}} allowFullScreen="" loading="lazy" className={styles.mapping}
             referrerPolicy="no-referrer-when-downgrade"></iframe>
-        
+
         </>);
 };
 
